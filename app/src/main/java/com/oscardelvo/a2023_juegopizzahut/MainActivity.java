fa703ba6c9f3a1a92017569813e294794290ac5c
OscarDelvoDS <odelvo@display-solutions.cr>
2023-03-24 13:15:26 -0600

    Initial commit

---
+++ app/src/main/java/com/oscardelvo/a2023_juegopizzahut/MainActivity.java
@@ -0,0 +1,14 @@
+package com.oscardelvo.a2023_juegopizzahut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
